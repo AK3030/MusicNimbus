0c54d483ae65ae4d6f2c861993d98f1e9d0fca32
Alex Kite <alexkite511@gmail.com>
2018-01-31 10:59:00 -0800

    add app and root components, add react router, update entry file

--- frontend/nimbus.jsx
+++ frontend/nimbus.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// import {login, signup, logout} from './util/session_api_util';
-
 import configureStore from './store/store';
+import Root from './components/root';
 
-import {login, signup, logout} from './actions/session_actions';
+// import {login, signup, logout} from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
-  window.login = login;
-  window.signup = signup;
-  window.logout = logout;
-  window.store = configureStore();
+
+  const store = configureStore();
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Hi</h1>, root);
+  ReactDOM.render(<Root store={ store }/>, root);
 });
