606c6052c8cd73082d8c8598a658f52e7715a5be
Alex Kite <alexkite511@gmail.com>
2018-02-01 20:53:13 -0800

    style navbar

--- frontend/components/navbar.jsx
+++ frontend/components/navbar.jsx
@@ -3,23 +3,26 @@ import { Link } from 'react-router-dom';
 
 const NavBar = ({currentUser}) => {
   return (
-    <header className="main-header">
-      <nav className="main-navbar">
-        <ul className="header-list">
-          <li><a href="#/navbar">Nimbus</a></li>
-          <li><a href="#/navbar">Home</a></li>
-          <li><a href="#/navbar">Collection</a></li>
-        </ul>
-        <form id="search-form">
-          <input id="search-box" placeholder="Search"></input>
-        </form>
-        <ul className="header-list">
-          <li><a href="#/navbar">Upgrade</a></li>
-          <li><a href="#/navbar">Upload</a></li>
-          <li><a href="#/navbar">Alex Kite</a></li>
-        </ul>
-      </nav>
-    </header>
+    <div>
+      <header className="main-header">
+        <nav className="main-navbar">
+          <ul className="header-list">
+            <li><a id="nimbus-link"href="#/navbar">Nimbus</a></li>
+            <li><a id="home-link" href="#/navbar">Home</a></li>
+            <li><a id="collection-link" href="#/navbar">Collection</a></li>
+          </ul>
+          <form id="search-form">
+            <input id="search-box" placeholder="Search"></input>
+          </form>
+          <ul className="header-list">
+            <li><a id="upgrade-link" href="#/navbar">Upgrade</a></li>
+            <li><a href="#/navbar">Upload</a></li>
+            <li><a href="#/navbar">Alex Kite</a></li>
+          </ul>
+        </nav>
+      </header>
+      <div class="test-thing"></div>
+    </div>
 
   );
 };
