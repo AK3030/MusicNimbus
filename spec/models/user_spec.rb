158b3432a449fa853b4682f5f7566ed1f20d99dd
Alex Kite <alexkite511@gmail.com>
2018-04-05 16:59:53 -0700

    add one user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # What to test?
+  # Validations
+  # Associations
+  # Class Methods
+
+  describe 'validations' do
+    it 'should validate presence of username' do
+      user = User.new(password:'bladerunner')
+      expect(user.valid?).to be false
+    end 
+
+  end
+
+  # describe 'associations'
+  #
+  # describe 'class methods'
 end
