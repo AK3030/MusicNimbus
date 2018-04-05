7348c1222241d9b47f1062cf957a350c334cc2fb
Alex Kite <alexkite511@gmail.com>
2018-04-05 16:51:30 -0700

    fix application.rb and add pending test for user model

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Nimbus
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
 
     # We have added this in.
     config.generators do |g|
