ebc47c5182176f1fcee971f82e56a7597423dfbb
Alex Kite <alexkite511@gmail.com>
2018-04-20 15:34:34 -0700

    delete audio validation

--- app/models/track.rb
+++ app/models/track.rb
@@ -24,7 +24,7 @@ class Track < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-  has_attached_file :audio
+  has_attached_file :audio, validate_media_type: false
   validates_attachment_presence :audio
   validates_attachment_content_type :audio, :content_type => [ 'audio/mpeg', 'audio/x-mpeg', 'audio/mp3', 'audio/x-mp3', 'audio/mpeg3', 'audio/x-mpeg3', 'audio/mpg', 'audio/x-mpg', 'audio/x-mpegaudio' ]
 
